2e1383371c2eaa28ce73bc2e36ba0e4499b44791
Yosy <yoselynespinoza27@gmail.com>
2025-04-21 21:44:59 -0500

    Initial commit

---
+++ src/main/java/com/proyecto/proyecto_farmasol/ProyectoFarmasolApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyecto_farmasol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFarmasolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFarmasolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/proyecto_farmasol/ProyectoFarmasolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.proyecto_farmasol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFarmasolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

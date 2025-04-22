40e5a3544e03e470af00a600f0652d9664e9e7ca
Carlos Roncal <roncalhermenegildo@gmail.com>
2025-04-22 00:08:54 -0500

    Usuario Categoria

---
+++ src/main/java/com/proyecto/proyecto_farmasol/model/CategoriaEntity.java
@@ -0,0 +1,25 @@
+package com.proyecto.proyecto_farmasol.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_categorias")
+public class CategoriaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(length = 255)
+    private String descripcion;
+
+}
